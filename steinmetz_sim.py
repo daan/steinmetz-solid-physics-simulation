5d7145fcfe439b38d26c4d0bc0b9422dfaa5118d
Daniel Saakes <daniel@saakes.net>
2022-05-25 15:47:02 +0200

    bicylinder works good!

--- steinmetz_sim.py
+++ steinmetz_sim.py
@@ -10,17 +10,17 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 
 p.setGravity(0,0,-10)
 
-p.resetDebugVisualizerCamera(cameraDistance=1, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=[0, 0, 0.5])
+p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=80, cameraPitch=-30, cameraTargetPosition=[0, 1.5, 0.5])
 
 
 planeId = p.loadURDF("models/plane.urdf")
 
-startPos = [0,0,0.30]
-startOrientation = p.getQuaternionFromEuler([0,math.radians(90),0])
-sphereId = p.loadURDF("models/cylinder.urdf",startPos, startOrientation)
+startPos = [0,0,0.6]
+startOrientation = p.getQuaternionFromEuler([math.radians(90),0,0])
+sphereId = p.loadURDF("models/bicylinder.urdf",startPos, startOrientation)
 
 #                                  force       position    or LINK_FRAME
-p.applyExternalForce(sphereId, -1, [0, 20, 0], [0,0,0.3], p.WORLD_FRAME)
+p.applyExternalForce(sphereId, -1, [0, 20, 0], [0,0,0.5], p.WORLD_FRAME)
 
 while True:
     try:
